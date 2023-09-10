738f4ac0a594c8a5e6499565d176fbe208c6569d
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-10 21:18:00 +0530

    Time: 2 ms (23.74%), Space: 40.7 MB (89.59%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxPower(String s) {
+        int count=1;
+        int res=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+                res=Math.max(count,res);
+            }
+            else count=1;
+        }
+        return res;
+        
+    }
+}
