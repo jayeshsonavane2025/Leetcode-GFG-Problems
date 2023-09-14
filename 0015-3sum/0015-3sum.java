7498d0886eec66bba62c033847c39ba7f240c68e
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-14 19:45:12 +0530

    Time: 41 ms (46.07%), Space: 50.8 MB (50.46%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,49 +1,52 @@
-class Solution {
-    //3 sum problem
+class Solution { 
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
-                }
-            }
-        }
-        return ans;
+        //You can code here
+      Arrays.sort(nums);
+      List<List<Integer>> list=new ArrayList<>();
+      for(int i=0;i<nums.length-1;i++){
+        if(i>0 && nums[i]==nums[i-1])continue;
+        List<List<Integer>> temp=twoSum(nums,-nums[i],i+1);
+        Iterator it=temp.iterator();
+        while(it.hasNext()){
+           List<Integer> temp2=(List)it.next();
+           temp2.add(0,nums[i]);
+           list.add(temp2);
+        } 
+      }
+      return list;
     }
+   public List<List<Integer>> twoSum(int[] nums,int target,int i){
+     List<List<Integer>> list=new ArrayList<>();
+     int start=i;
+     int end=nums.length-1;
+     while(start<end){
+       if(start>i && nums[start]==nums[start-1]){
+         start++;
+         continue;
+       }
+       if(end<nums.length-1 && nums[end]==nums[end+1]){
+         end--;
+         continue;
+       }
+       int sum=nums[start]+nums[end];
+       if(sum==target){
+         List<Integer> temp=new ArrayList<>();
+         temp.add(nums[start]);
+         temp.add(nums[end]);
+         list.add(temp);
+         start++;
+         end--;
+         
+       }
+       else if(sum>target){
+         end--;
+       }
+       else{
+         start++;
+       }
+     }
+     return list;
+   }
+  
 }
+ 
