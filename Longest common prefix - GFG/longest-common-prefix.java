5a0c51d9c5e3a400ca3dd2ad4527713604db3b10
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-16 08:36:00 +0530

    Added solution - LeetHub

---
+++ Longest common prefix - GFG/longest-common-prefix.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            String s1 = sc.next();
+            String s2 = sc.next();
+            Solution obj = new Solution();
+            int[] ans=obj.longestCommonPrefix(s1,s2);
+            if(ans[0] == -1)
+                System.out.println(ans[0]);
+            else 
+                System.out.println(ans[0]+" "+ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int[] longestCommonPrefix(String s1, String s2){
+        //code here
+        int[] a=new int[2];
+        for(int i=0;i<s1.length();i++){
+            if(s2.contains(s1.substring(0,i))){
+                a[0]=0;
+                a[1]=i-1;
+            }
+        }
+        return a;
+    }
+}
