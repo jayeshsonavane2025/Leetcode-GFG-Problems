b405ededf7af617d094e69a38da89e07b84f994d
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-19 12:04:20 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (21.14%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -1,21 +1,27 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        //Form right min array
-        int rmin[]=new int[arr.length+1];
-        rmin[arr.length]=Integer.MAX_VALUE;
-        for(int i=arr.length-1;i>=0;i--){
-            rmin[i]=Math.min(rmin[i+1],arr[i]);
+        if (arr.length == 0) return 0; // If the array is empty, return 0 chunks.
+
+        int[] maxFromStart = new int[arr.length]; // Stores the maximum value encountered from the start.
+        int[] minFromEnd = new int[arr.length];   // Stores the minimum value encountered from the end.
+
+        maxFromStart[0] = arr[0]; // Initialize the first element of maxFromStart.
+        for (int i = 1; i < arr.length; i++) {
+            maxFromStart[i] = Math.max(maxFromStart[i - 1], arr[i]); // Calculate max values from the start.
         }
-        
-        //Generating left max array and keeping track of count
-        int lmax=Integer.MIN_VALUE;
-        int c=0;
-        for(int i=0;i<arr.length;i++){
-            lmax=Math.max(lmax,arr[i]);
-            if(lmax<=rmin[i+1])
-                c++;
+
+        minFromEnd[arr.length - 1] = arr[arr.length - 1]; // Initialize the last element of minFromEnd.
+        for (int i = arr.length - 2; i >= 0; i--) {
+            minFromEnd[i] = Math.min(minFromEnd[i + 1], arr[i]); // Calculate min values from the end.
         }
-        return c;
+
+        int chunks = 1; // Initialize the chunk count to 1.
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (maxFromStart[i] <= minFromEnd[i + 1]) {
+                chunks++; // If max from start is less than or equal to min from end, we can form a new chunk.
+            }
+        }
+
+        return chunks; // Return the maximum number of chunks.
     }
-    
 }
