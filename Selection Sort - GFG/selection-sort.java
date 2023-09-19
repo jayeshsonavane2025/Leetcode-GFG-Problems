a84f1376b61bc1008dc35d746d4cf3c9bde85625
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-19 21:23:30 +0530

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	void  select(int arr[], int i)
+	{
+        // code here such that selectionSort() sorts arr[]
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	   for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+	}
+}
