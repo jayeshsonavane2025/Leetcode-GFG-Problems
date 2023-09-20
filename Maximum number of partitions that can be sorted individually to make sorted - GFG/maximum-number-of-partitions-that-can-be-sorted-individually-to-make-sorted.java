5c2e033b642086cb08ae3ec13c051bde11539eab
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-09-20 07:23:10 +0530

    Added solution - LeetHub

---
+++ Maximum number of partitions that can be sorted individually to make sorted - GFG/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Complete obj = new Complete();
+		    int ans = obj.maxPartitions(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int maxPartitions (int a[], int n) {
+        //Complete the function
+        int max=a[0];
+        int chunks=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,a[i]);
+            if(i==max)chunks++;
+        }
+        return chunks;
+    } 
+}
+
+
