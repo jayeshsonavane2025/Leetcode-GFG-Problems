10932aecf63dcfe69fe90dbfbc0059401253d863
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-10-01 14:55:50 +0530

    Added solution - LeetHub

---
+++ Implement stack using array - GFG/implement-stack-using-array.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+
+import java.io.*;
+import java.lang.*;
+
+class GfG {
+
+	public static void main(String args[])throws IOException
+	{
+// 		 Scanner sc = new Scanner(System.in);
+// 		 int t=sc.nextInt();
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+         int t = Integer.parseInt(read.readLine());
+         
+		 while(t>0)
+		 {
+			MyStack obj = new MyStack();
+			int Q = Integer.parseInt(read.readLine());
+			int k = 0;
+			String str[] = read.readLine().trim().split(" ");
+			while(Q>0)
+			{
+				int QueryType = 0;
+				QueryType = Integer.parseInt(str[k++]);
+				if(QueryType == 1)
+				{
+					int a = Integer.parseInt(str[k++]);
+					
+					obj.push(a);
+					
+				}else if(QueryType == 2)
+				{
+				System.out.print(obj.pop()+" ");
+				}
+				Q--;
+			}
+			System.out.println("");
+			t--;
+		 }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class MyStack {
+    int top;
+    int arr[];
+
+    MyStack() {
+        top = -1;
+        arr = new int[1000];
+    }
+
+    void push(int a) {
+      //You can code here
+      top++;
+      arr[top]=a; 
+    }
+
+    int pop() {
+        //You can code here
+        if(top==-1){
+            return -1;
+        }
+        else{
+            return arr[top--];
+            
+        }
+
+
+      
+    }
+}
