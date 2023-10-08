88c2bf8053a04635e71fe635c7b00b2e6fb59379
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-10-08 11:23:21 +0530

    Time: 2 ms (96.25%), Space: 41.2 MB (44.12%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,39 +1,28 @@
 class Solution {
-    public String removeDuplicateLetters(String s) {
-        int[] count = new int[26]; // Count of each character in the string
-        boolean[] inStack = new boolean[26]; // Tracks if a character is already in  the stack
-        Stack<Character> stack = new Stack<>(); // Stack to build the result
-
-        // Count the frequency of each character in the string
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-
-        // Process each character in the string
-        for (char c : s.toCharArray()) {
-            count[c - 'a']--; // Decrease the count of the current character
-
-            // Skip characters that are already in the stack
-            if (inStack[c - 'a']) {
-                continue;
-            }
-
-            // Remove characters from the stack if they are greater and still have  occurrences remaining
-            while (!stack.isEmpty() && c < stack.peek() && count[stack.peek() - 'a'] > 0) {
-                inStack[stack.pop() - 'a'] = false;
-            }
-
-            // Add the current character to the stack
-            stack.push(c);
-            inStack[c - 'a'] = true;
-        }
-
-        // Build the result string from the stack
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-
-        return result.toString();
+ public String removeDuplicateLetters(String s) {
+    Stack<Character> stack = new Stack<>();
+    int[] count = new int[26];
+    char[] arr = s.toCharArray();
+    for(char c : arr) {
+        count[c-'a']++;
     }
+    boolean[] visited = new boolean[26];
+    for(char c : arr) {
+        count[c-'a']--;
+        if(visited[c-'a']) {
+            continue;
+        }
+        while(!stack.isEmpty() && stack.peek() > c && count[stack.peek()-'a'] > 0) {
+            visited[stack.peek()-'a'] = false;
+            stack.pop();
+        }
+        stack.push(c);
+        visited[c-'a'] = true;
+    }
+    StringBuilder sb = new StringBuilder();
+    for(char c : stack) {
+        sb.append(c);
+    }
+    return sb.toString();
+}
 }
