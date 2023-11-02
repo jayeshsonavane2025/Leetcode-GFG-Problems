019210d6ca32d3441464baa0c81f6d3b139a8346
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2023-11-02 08:26:16 +0530

    Time: 1 ms (98.34%), Space: 41.3 MB (34.76%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int titleToNumber(String s) {
+        // if(s==null){
+        //     return -1;
+        // }
+        int sum=0;
+        s.toUpperCase();
+        for(char c:s.toCharArray()){
+            sum*=26;
+            sum+=c-'A'+1;
+        }
+        return sum;
+    }
+}
