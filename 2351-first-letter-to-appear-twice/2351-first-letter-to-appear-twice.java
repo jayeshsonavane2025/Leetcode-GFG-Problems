e383c7aa4f389865ab185d20cd280055935d2743
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-08-02 13:42:10 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (72.71%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,15 +1,10 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        int appear = 0;
-        
-        int mask = 0;
-        for (int i = 0; i < s.length(); i++) {
-            mask = 1 << (s.charAt(i) - 'a');
-            if ((appear & mask) != 0) {
-                return s.charAt(i);
-            }
-            appear |= mask;
-        }
-        return '-';
+        Set<Character> set = new HashSet();
+        for (char c : s.toCharArray())  
+            if (!set.add(c)) 
+                return c;
+				
+        return 'a';// can't reach to this line, because there must be a letter appearing TWICE
     }
 }
