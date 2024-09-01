07516b3d5484eaed8c0a769cf810d441263d2ab6
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-09-01 09:06:32 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (9.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,20 @@
-class Solution {
-	public int rob(int[] nums) {
-		int n = nums.length;
-		int[] include = new int[n];
-		int[] exclude = new int[n];
-		include[0] = nums[0];
-		exclude[0] = 0;
-		for (int i = 1; i < n; i++) {
-			include[i] = nums[i] + exclude[i - 1];
-			exclude[i] = Math.max(include[i - 1], exclude[i - 1]);
-		}
-		return Math.max(include[n - 1], exclude[n - 1]);
-	}
+ class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        int dp[] = new int[n];
+        dp[0]=nums[0];
+        if(n==1){
+            return nums[0];
+        }
+        
+        dp[1] = Math.max(nums[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        
+        return dp[n-1];
+
+    }
 }
