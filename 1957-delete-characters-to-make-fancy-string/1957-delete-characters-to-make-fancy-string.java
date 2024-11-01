f8db58440eb426438dd6eef57199147dbbc47480
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-11-01 08:33:23 +0530

    Time: 38 ms (43.02%), Space: 45.6 MB (47.62%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -1,31 +1,19 @@
 class Solution {
     public String makeFancyString(String s) {
-        Stack<Character> st=new Stack();
-        int count=0;
-        for(char c:s.toCharArray()){
-           if(!st.isEmpty() && st.peek()==c && count==2){
-            //if there are more than 2 characters dont add to stack
-            continue;
-        }
-        
-            else if(!st.isEmpty() && st.peek()!=c)
-                //if there are different characters then add to stack and change value of count to 1
-            {
-                st.push(c);
-                count=1;
+        StringBuilder ans = new StringBuilder();
+        ans.append(s.charAt(0));
+        int n = s.length(), cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == ans.charAt(ans.length() - 1)) {
+                cnt++;
+                if (cnt < 3) {
+                    ans.append(s.charAt(i));
+                }
+            } else {
+                cnt = 1;
+                ans.append(s.charAt(i));
             }
-        
-            else//else add to stack and increase count
-            { 
-                st.push(c);
-               count++;
         }
-    }
-
-        StringBuilder sb=new StringBuilder();
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-        return sb.reverse().toString();
+        return ans.toString();
     }
 }
