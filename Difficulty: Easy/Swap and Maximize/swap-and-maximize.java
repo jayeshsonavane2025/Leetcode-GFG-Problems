7660068d256e3cd457e3d0065937faaa3824670a
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-11-01 08:19:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap and Maximize/swap-and-maximize.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+// Driver class
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public long maxSum(Long[] arr) {
+        // code here
+        Arrays.sort(arr);
+         long sum=0;
+         int i=0;
+         int j=arr.length-1;
+         while(i <= j){
+              sum+=2*arr[j];
+              sum-=2*arr[i];
+              i++;
+              j--;
+         }
+         
+         return sum;
+    }
+}
+
+
+//{ Driver Code Starts.
+class Main {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the Long integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into Long integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            // Convert ArrayList to array
+            Long[] arr = new Long[array.size()];
+            array.toArray(arr);
+
+            Solution ob = new Solution();
+
+            // Call maxSum method and print result
+            long ans = ob.maxSum(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
