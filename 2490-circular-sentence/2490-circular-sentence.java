398b2fb09188c6b20dd94ca5628e2ba956189937
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-11-02 17:14:20 +0530

    Time: 1 ms (96.81%), Space: 41.9 MB (72.21%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
         if(sentence.charAt(0) != sentence.charAt(sentence.length()-1))
-        return false;
+            return false;
         for(int i=1;i<sentence.length()-1;i++){
             if(sentence.charAt(i) == ' '){
                 if(sentence.charAt(i-1) != sentence.charAt(i+1))
