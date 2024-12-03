534ac16ede836f620f9d596b9e6191fd576b0a92
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2024-12-03 12:47:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,24 +1,19 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNumber(arr));
+            System.out.println("~");
         }
     }
 }
@@ -29,24 +24,19 @@ class GFG {
 
 
 
-// User function Template for Java
+
+
 class Solution {
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, int arr[]) {
-
-         int val = 0;
-
-        // XOR all numbers from 1 to n
-        for (int i = 1; i <= n; i++) {
-            val = val ^ i;
+    int missingNumber(int arr[]) {
+        // code here
+        int n = arr.length + 1;
+        
+        int sum = (n * (n + 1)) / 2;
+        
+        for(int i = 0; i < arr.length; i++) {
+            sum -= arr[i];
         }
-
-        // XOR all elements in the array
-        for (int i = 0; i < n - 1; i++) {
-            val = val ^ arr[i];
-        }
-
-        return val;
+        
+        return sum;
     }
 }
