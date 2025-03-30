c1d51e8b7a3222f14073b39baa39c4338769baad
Jayesh Sonavane <jayeshsonavane2025@gmail.com>
2025-03-30 16:32:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -21,6 +21,8 @@ public class GFG {
             Solution ob = new Solution();
             int res = ob.binarysearch(arr, k);
             System.out.println(res);
+
+            System.out.println("~");
         }
         sc.close();
     }
@@ -29,26 +31,29 @@ public class GFG {
 // } Driver Code Ends
 
 
+
+
 // User function Template for Java
 
 class Solution {
     public int binarysearch(int[] arr, int k) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == k) {
-                return mid;
-            } else if (arr[mid] < k) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return -1;  // If the element is not found
+       
+       int left=0;
+       int right=arr.length-1;
+       int result=-1;
+       while(left<=right){
+           int mid =left+(right-left)/2;
+           if(arr[mid]==k){
+               result=mid;
+               right=mid-1;
+               
+           }
+           if(arr[mid]<k){
+               left=mid+1;
+           }else{
+               right=mid-1;
+           }
+       }
+       return result;
     }
 }
-
